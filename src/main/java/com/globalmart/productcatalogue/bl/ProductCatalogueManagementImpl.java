f9932a7f00d0f91ea815971dedec487d3c79d0e0
Souvik Goswami <souvik.goswami@innoviti.com>
2018-01-19 20:59:51 +0530

    Changed name of variables.

--- src/main/java/com/globalmart/productcatalogue/bl/ProductCatalogueManagementImpl.java
+++ src/main/java/com/globalmart/productcatalogue/bl/ProductCatalogueManagementImpl.java
@@ -17,19 +17,19 @@ import com.globalmart.productcatalogue.model.entity.Product;
  */
 public class ProductCatalogueManagementImpl implements ProductCatalogueManagement {
 
-    private static ProductCatalogueManager opr = new DatabaseOperation();
+    private ProductCatalogueManager manager = new DatabaseOperation();
 
     @Override
     public int addProduct(ProductDTO dto) {
 	// Convert the dto to entity object
 	Product product = new Product(dto.getName(), dto.getType(), dto.getCode(), dto.getPrice());
 	// Persist the entity object
-	return opr.insert(product);
+	return manager.insert(product);
     }
 
     @Override
     public ProductDTO getProduct(int id) {
-	Product bdo = opr.read(id);
+	Product bdo = manager.read(id);
 	ProductDTO result = null;
 	if (bdo != null)
 	    result = new ProductDTO(bdo.getId(), bdo.getName(), bdo.getType(), bdo.getCode(), bdo.getPrice());
@@ -38,17 +38,17 @@ public class ProductCatalogueManagementImpl implements ProductCatalogueManagemen
 
     @Override
     public List<ProductDTO> searchProduct(String criteria, String value) {
-	List<Product> pList = opr.search(criteria, value);
-	List<ProductDTO> listOfProducts = new ArrayList<>();
-	for (Product bdo : pList)
-	    listOfProducts
+	List<Product> listOfProducts = manager.search(criteria, value);
+	List<ProductDTO> listOfProductDto = new ArrayList<>();
+	for (Product bdo : listOfProducts)
+	    listOfProductDto
 		    .add(new ProductDTO(bdo.getId(), bdo.getName(), bdo.getType(), bdo.getCode(), bdo.getPrice()));
-	return listOfProducts;
+	return listOfProductDto;
     }
 
     @Override
     public boolean deleteProduct(int id) {
-	return opr.delete(id);
+	return manager.delete(id);
     }
 
 }
