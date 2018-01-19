98429c8adf1e86f82c3252b90894bd7a4d6052e0
Souvik Goswami <souvik.goswami@innoviti.com>
2018-01-19 21:12:00 +0530

    Concise formatting.

--- src/main/java/com/globalmart/productcatalogue/service/ProductCatalogService.java
+++ src/main/java/com/globalmart/productcatalogue/service/ProductCatalogService.java
@@ -26,126 +26,126 @@ import com.globalmart.productcatalogue.bl.dto.ProductDTO;
 @Path("/productcatalogue")
 public class ProductCatalogService {
 
-	private static Logger LOG = Log.getLogger(ProductCatalogService.class);
-	private static ProductCatalogueManagement pcm = new ProductCatalogueManagementImpl();
+    private static Logger LOG = Log.getLogger(ProductCatalogService.class);
+    private static ProductCatalogueManagement pcm = new ProductCatalogueManagementImpl();
 
-	/**
-	 * Adds a product
-	 */
-	@POST
-	@Path("/add")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response addProduct(ProductDTO product) {
-		Response response = null;
-		try {
-			int id = pcm.addProduct(product);
-			LOG.info("Product Added with id: " + id);
-			response = Response.created(URI.create("/productcatalogue/get/" + id)).build();
-		} catch (Exception e) {
-			LOG.info(e);
-			response = Response.serverError().entity("Exception in adding product: " + e.getMessage()).build();
-		}
-		return response;
+    /**
+     * Adds a product
+     */
+    @POST
+    @Path("/add")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addProduct(ProductDTO product) {
+	Response response = null;
+	try {
+	    int id = pcm.addProduct(product);
+	    LOG.info("Product Added with id: " + id);
+	    response = Response.created(URI.create("/productcatalogue/get/" + id)).build();
+	} catch (Exception e) {
+	    LOG.info(e);
+	    response = Response.serverError().entity("Exception in adding product: " + e.getMessage()).build();
 	}
+	return response;
+    }
 
-	/**
-	 * Gets a product based on id
-	 * 
-	 * @param id
-	 * @return
-	 */
-	@GET
-	@Path("/get/{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getProduct(@PathParam("id") int id) {
-		Response response = null;
-		try {
-			ProductDTO result = pcm.getProduct(id);
-			if (result != null) {
-				LOG.info("Product Found with id: " + id);
-				response = Response.ok().entity(result).build();
-			} else {
-				LOG.info("Could not find product with id: " + id);
-				response = Response.noContent().entity("Product with given id is not found").build();
-			}
-		} catch (Exception e) {
-			LOG.info(e);
-			response = Response.serverError()
-					.entity("Exception in fetching the product with given id: " + e.getMessage()).build();
-		}
-		return response;
+    /**
+     * Gets a product based on id
+     * 
+     * @param id
+     * @return
+     */
+    @GET
+    @Path("/get/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getProduct(@PathParam("id") int id) {
+	Response response = null;
+	try {
+	    ProductDTO result = pcm.getProduct(id);
+	    if (result != null) {
+		LOG.info("Product Found with id: " + id);
+		response = Response.ok().entity(result).build();
+	    } else {
+		LOG.info("Could not find product with id: " + id);
+		response = Response.noContent().entity("Product with given id is not found").build();
+	    }
+	} catch (Exception e) {
+	    LOG.info(e);
+	    response = Response.serverError()
+		    .entity("Exception in fetching the product with given id: " + e.getMessage()).build();
 	}
+	return response;
+    }
 
-	/**
-	 * deletes a product based on id
-	 * 
-	 * @param id
-	 * @return
-	 */
-	@DELETE
-	@Path("/remove/{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response deleteProduct(@PathParam("id") int id) {
-		Response response = null;
-		try {
-			boolean result = pcm.deleteProduct(id);
-			if (result) {
-				LOG.info("Deleted the product with id: " + id);
-				response = Response.ok().build();
-			} else {
-				LOG.info("No Product found with id: " + id);
-				response = Response.noContent().entity("Product with given id is not found").build();
-			}
-		} catch (Exception e) {
-			LOG.info(e);
-			response = Response.serverError()
-					.entity("Exception in deleting the product with given id: " + e.getMessage()).build();
-		}
-		return response;
+    /**
+     * deletes a product based on id
+     * 
+     * @param id
+     * @return
+     */
+    @DELETE
+    @Path("/remove/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteProduct(@PathParam("id") int id) {
+	Response response = null;
+	try {
+	    boolean result = pcm.deleteProduct(id);
+	    if (result) {
+		LOG.info("Deleted the product with id: " + id);
+		response = Response.ok().build();
+	    } else {
+		LOG.info("No Product found with id: " + id);
+		response = Response.noContent().entity("Product with given id is not found").build();
+	    }
+	} catch (Exception e) {
+	    LOG.info(e);
+	    response = Response.serverError()
+		    .entity("Exception in deleting the product with given id: " + e.getMessage()).build();
 	}
+	return response;
+    }
 
-	/**
-	 * Retrieves a list of products based on given search criteria and only
-	 * code, name or type can be used as search criteria
-	 * 
-	 * @param key
-	 * @param value
-	 * @return
-	 */
-	@GET
-	@Path("/search")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response searchProducts(@QueryParam("key") String key, @QueryParam("value") String value) {
-		ObjectMapper mapper = new ObjectMapper();
-		String pList = "";
-		Response response = null;
-		try {
-			List<ProductDTO> products = pcm.searchProduct(key, value);
-			if (products == null || products.isEmpty()) {
-				LOG.info("Products with given query parameters are not found");
-				response = Response.noContent().entity("No Content Found").build();
-			} else {
-				LOG.info("Products with given query parameters are found");
-				pList = mapper.writeValueAsString(products);
-				response = Response.ok().entity(pList).build();
-			}
-		} catch (JsonProcessingException e) {
-			LOG.info(e);
-			response = Response.serverError()
-					.entity("Exception in searching the product with given query parameters: " + e.getMessage())
-					.build();
-		}
-		return response;
+    /**
+     * Retrieves a list of products based on given search criteria and only
+     * code, name or type can be used as search criteria
+     * 
+     * @param key
+     * @param value
+     * @return
+     */
+    @GET
+    @Path("/search")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response searchProducts(@QueryParam("key") String key, @QueryParam("value") String value) {
+	ObjectMapper mapper = new ObjectMapper();
+	String pList = "";
+	Response response = null;
+	try {
+	    List<ProductDTO> products = pcm.searchProduct(key, value);
+	    if (products == null || products.isEmpty()) {
+		LOG.info("Products with given query parameters are not found");
+		response = Response.noContent().entity("No Content Found").build();
+	    } else {
+		LOG.info("Products with given query parameters are found");
+		pList = mapper.writeValueAsString(products);
+		response = Response.ok().entity(pList).build();
+	    }
+	} catch (JsonProcessingException e) {
+	    LOG.info(e);
+	    response = Response.serverError()
+		    .entity("Exception in searching the product with given query parameters: " + e.getMessage())
+		    .build();
 	}
+	return response;
+    }
 
-	/**
-	 * Test API to check server is up or not
-	 */
-	@GET
-	@Path("/test")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String testApp() {
-		return "Hello! I am up and running";
-	}
+    /**
+     * Test API to check server is up or not
+     */
+    @GET
+    @Path("/test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String testApp() {
+	return "Hello! I am up and running";
+    }
 }
